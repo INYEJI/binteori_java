702038b3361b32792c4d194e7243bc029b020bca
INYEJI <gyutree@naver.com>
2025-02-28 18:00:56 +0900

    공

---
+++ Java20250304_반복문/src/ch04/ex01/ForExam3.java
@@ -0,0 +1,16 @@
+package ch04.ex01;
+
+public class ForExam3 {
+
+	public static void main(String[] args) {
+
+		int sum = 0;
+		
+		for(int i=1; i<=100 ; i++) { //1부터 100까지 반복
+			if(i%2 == 1) { //홀수인지묻는중 
+				sum += i; //홀수이면 누적
+			}
+		}
+		System.out.println("sum = " + sum);
+	}
+}
